d9573c202516849abb526a315610b3caeb3d303b
MikeJpaDev <168370816+MikeJpaDev@users.noreply.github.com>
2025-06-04 23:29:56 -0400

    terminado actividad

--- src/main.scala
+++ src/main.scala
@@ -3,10 +3,10 @@ import org.mindrot.jbcrypt.BCrypt
 import services.{ActividadService, AnimalService, ContratoService, ProvAlimentoService, ProvComplementarioService, ProveedorService, ServicioService, UserService, VeterinarioService}
 import utils.{DatabaseConnection, Utils}
 
-import java.sql.Date
+import java.sql.{Date, Timestamp}
 import scala.util.{Failure, Success}
 
 @main
 def main(): Unit =
-  val actividades = ActividadService.getAllActividad
-  actividades.foreach(println)
+  val actividades = ActividadService.updateActividad("49c09e75-ea56-4b96-bab2-4c6f8c638c00", "1b26cbf1-d1cd-48d9-8cc6-dc4f368f7b9d", 19, "la misma de siemore",Timestamp.valueOf("2025-12-10 20:10:00"))
+  println(actividades)
--- src/services/ActividadService.scala
+++ src/services/ActividadService.scala
@@ -2,8 +2,10 @@ package services
 import models.Actividad
 import utils.DatabaseConnection
 
-import java.sql.{PreparedStatement, ResultSet, SQLException}
+import java.sql.{PreparedStatement, ResultSet, SQLException, Timestamp}
+import java.util.UUID
 import scala.collection.mutable.ListBuffer
+import scala.util.Using
 
 object ActividadService {
   def getAllActividad: List[Actividad] = {
@@ -40,6 +42,134 @@ object ActividadService {
         throw new SQLException(s"Error obteniendo actividades: ${e.getMessage}", e)
       case e: Exception =>
         throw new Exception(s"Error inesperado obteniendo actividades: ${e.getMessage}", e)
+    }
+  }
+
+  def createActividad(
+                       animalId: String,
+                       servicioId: Int,
+                       description: String,
+                       horario: Timestamp
+                     ): Actividad = {
+    try {
+      val uuid = UUID.fromString(animalId)
+      DatabaseConnection.withConnection { conn =>
+        val query = "{call public.create_actividad(?, ?, ?, ?:: Timestamp)}"
+        Using.resource(conn.prepareCall(query)) { stmt =>
+          stmt.setObject(1, uuid)
+          stmt.setInt(2, servicioId)
+          stmt.setString(3, description)
+          stmt.setTimestamp(4, horario)
+
+          val rs = stmt.executeQuery()
+          if (rs.next()) {
+            Actividad(
+              rs.getString("actividad_id"),
+              AnimalService.getAnimalById(rs.getString("animal_id_fk")).nombre_animal,
+              rs.getString("animal_id_fk"),
+              rs.getInt("servicio_id_fk"),
+              rs.getString("descripcion"),
+              rs.getTimestamp("horario")
+            )
+          } else {
+            throw new SQLException("No se pudo crear la actividad: sin datos de retorno")
+          }
+        }
+      }
+    } catch {
+      case e: SQLException =>
+        throw new SQLException(s"Error creando actividad: ${e.getMessage}", e)
+    }
+  }
+
+
+  def deleteActividad(actividadId: String): Boolean = {
+    try {
+      val uuid = UUID.fromString(actividadId)
+      DatabaseConnection.withConnection { conn =>
+        val query = "{call public.delete_actividad(?)}"
+        Using.resource(conn.prepareCall(query)) { stmt =>
+          stmt.setObject(1, uuid)
+          val rs = stmt.executeQuery()
+          if (rs.next()) rs.getBoolean(1)
+          else throw new InternalError("No se pudo retornar el valor")
+        }
+      }
+    } catch {
+      case e: SQLException =>
+        throw new SQLException(s"Error eliminando actividad: ${e.getMessage}", e)
+      case e: IllegalArgumentException =>
+        throw new IllegalArgumentException(s"Error de entrada de datos: ${e.getMessage}")
+      case e: Exception =>
+        throw new InternalError(s"Error Inesperado: ${e.getMessage}")
+    }
+  }
+
+  def getActividadById(actividadId: String): Actividad = {
+    try {
+      val uuid = UUID.fromString(actividadId)
+      DatabaseConnection.withConnection { conn =>
+        val query = "{call public.get_actividad_by_id(?)}"
+        Using.resource(conn.prepareCall(query)) { stmt =>
+          stmt.setObject(1, uuid)
+          val rs = stmt.executeQuery()
+
+          if (rs.next()) {
+            Actividad(
+              rs.getString("actividad_id"),
+              AnimalService.getAnimalById(rs.getString("animal_id_fk")).nombre_animal,
+              rs.getString("animal_id_fk"),
+              rs.getInt("servicio_id_fk"),
+              rs.getString("descripcion"),
+              rs.getTimestamp("horario")
+            )
+          } else {
+            throw InternalError("Error al obtener la entidad")
+          }
+        }
+      }
+    } catch {
+      case e: SQLException =>
+        throw new SQLException(s"Error obteniendo actividad: ${e.getMessage}", e)
+    }
+  }
+
+  def updateActividad(
+                       actividadId: String,
+                       animalId: String,
+                       servicioId: Int,
+                       description: String,
+                       horario: Timestamp
+                     ): Actividad = {
+    try {
+      val uuidActividad = UUID.fromString(actividadId)
+      val uuidAnimal = UUID.fromString(animalId)
+      DatabaseConnection.withConnection { conn =>
+        val query = "{call public.update_actividad(?, ?, ?, ?, ?)}"
+        Using.resource(conn.prepareCall(query)) { stmt =>
+          stmt.setObject(1, uuidActividad)
+          stmt.setObject(2, uuidAnimal)
+          stmt.setInt(3, servicioId)
+          stmt.setString(4, description)
+          stmt.setTimestamp(5, horario)
+
+          val rs = stmt.executeQuery()
+          if (rs.next()) {
+            Actividad(
+              rs.getString("actividad_id"),
+              AnimalService.getAnimalById(rs.getString("animal_id_fk")).nombre_animal,
+              rs.getString("animal_id_fk"),
+              rs.getInt("servicio_id_fk"),
+              rs.getString("descripcion"),
+              rs.getTimestamp("horario")
+            )
+          }
+          else throw new InternalError("Ocurrio un error al crear el objeto")
+        }
+      }
+    } catch {
+      case e: SQLException =>
+        throw new SQLException(s"Error actualizando actividad: ${e.getMessage}", e)
     }
   }
 }
